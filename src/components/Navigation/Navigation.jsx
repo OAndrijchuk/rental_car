09a8a4e66e8cd532166f4a412f7e3e7d5be52702
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-09-09 18:57:35 +0300

    mobMenuFix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { NavCont, NavLinkStyled } from './Navigation.styled';
+import { useDispatch } from 'react-redux';
+import { closeModal } from 'redux/carDetails/carDetailsSlice';
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   return (
     <NavCont>
-      <NavLinkStyled to="/">Home</NavLinkStyled>
-      <NavLinkStyled to="/catalog">Catalog</NavLinkStyled>
-      <NavLinkStyled to="/favorites">Favorites cars</NavLinkStyled>
+      <NavLinkStyled to="/" onClick={() => dispatch(closeModal())}>
+        Home
+      </NavLinkStyled>
+      <NavLinkStyled to="/catalog" onClick={() => dispatch(closeModal())}>
+        Catalog
+      </NavLinkStyled>
+      <NavLinkStyled to="/favorites" onClick={() => dispatch(closeModal())}>
+        Favorites cars
+      </NavLinkStyled>
     </NavCont>
   );
 };
